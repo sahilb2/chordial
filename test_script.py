05b80357375d4c3ca894ad9300157d6c64fe5cbd
sahilb2 <sahilbhatt@hotmail.com>
2019-03-02 16:46:27 -0500

    added search by track

--- test_script.py
+++ test_script.py
@@ -30,3 +30,26 @@ def get_artist_top_tracks(artist_name):
 
 artist_name = 'Taylor Swift'
 pp.pprint(get_artist_top_tracks(artist_name))
+
+def search_tracks_by_query(track_query):
+    results = spotify.search(q=track_query, type='track')
+    tracks = results['tracks']['items']
+    tracks_list = []
+    for track in tracks:
+        curr_track_info = {}
+        curr_track_info['name'] = track['name']
+        curr_track_info['uri'] = track['uri']
+        # Store artist names
+        artists = track['artists']
+        artist_name_list = []
+        for artist in artists:
+            curr_artist_name = artist['name']
+            artist_name_list.append(curr_artist_name)
+        curr_track_info['artists'] = artist_name_list
+        # Store album name
+        curr_track_info['album'] = track['album']['name']
+        tracks_list.append(curr_track_info)
+    return tracks_list
+
+track_query = 'Love Story'
+pp.pprint(search_tracks_by_query(track_query))
