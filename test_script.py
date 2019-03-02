e6dce286a0938c720fb9d06c6dc2e913b28e176d
sahilb2 <sahilbhatt@hotmail.com>
2019-03-02 16:14:40 -0500

    saving uri instead of id for more specific queries

--- test_script.py
+++ test_script.py
@@ -11,20 +11,20 @@ CLIENT_SECRET = KEYS['CLIENT_SECRET']
 client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-def get_artist_id(artist_name):
+def get_artist_uri(artist_name):
     results = spotify.search(q=artist_name, type='artist')
-    id = results['artists']['items'][0]['id']
-    return id
+    uri = results['artists']['items'][0]['uri']
+    return uri
 
 def get_artist_top_tracks(artist_name):
-    artist_id = get_artist_id(artist_name)
-    results = spotify.artist_top_tracks(artist_id)
+    artist_uri = get_artist_uri(artist_name)
+    results = spotify.artist_top_tracks(artist_uri)
     tracks = results['tracks']
     top_tracks_list = []
     for track in tracks:
         curr_track_info = {}
         curr_track_info['name'] = track['name']
-        curr_track_info['id'] = track['id']
+        curr_track_info['uri'] = track['uri']
         top_tracks_list.append(curr_track_info)
     return top_tracks_list
 
